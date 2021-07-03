fe9144986e7654208b9b23e9dd3ed48edc788234
shajeen ahamed <shajeenahmed@gmail.com>
2021-07-03 20:14:02 +0530

    changed folder name output to out. renamed setup

--- template_maker/main.py
+++ template_maker/main.py
@@ -16,9 +16,9 @@ def create_structure(config_file, project_name, option_type):
     with open(config_file) as file:
         data = json.load(file)
     dest = '/'
-    if os.path.exists('output'):
-        shutil.rmtree('output')
-    os.makedirs('output/')
+    if os.path.exists('out'):
+        shutil.rmtree('out')
+    os.makedirs('out/')
     dest = os.path.join(os.path.dirname(os.path.realpath(__file__)),'out',project_name,'')
     for d in data["folder"]:
         dir_d = dest+d["target"]+d["name"]
